a84c275dc0e53884099ea9e65f482a57258cbb39
kopz <gustavoeduardo438@gmail.com>
2024-07-30 19:00:14 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Consumindo API Spotify com OpenFeign
---
+++ src/test/java/org/example/spotifyapi/SpotifyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spotifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyapiApplicationTests {
+
+  @Test
+  void contextLoads () {
+  }
+
+}
